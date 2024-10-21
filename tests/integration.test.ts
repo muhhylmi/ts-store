c128ea7d6caf4f0947ff7598f5f6e0df3b6ab655
telkomdev-hylmi <hylmi.muh@gmail.com>
2024-10-21 14:36:31 +0700

    test: integration testing

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -27,21 +27,30 @@ afterAll(async () => {
 
 const app = express();
 app.use(express.json());
-app.post('/users', userHandler.createUser);
-app.get('/users', userHandler.getUser);
+app.post('/api/users',
+  (req, res, next) => userHandler.createUser(req, res, next)
+);
 
+app.get('/api/users', userHandler.getUser);
+const username = 'admin';
+const password = 'password';
+const encodeCredentials = (username, password) => {
+  return Buffer.from(`${username}:${password}`).toString('base64');
+};
+const credentials = encodeCredentials(username, password);
 jest.mock('../src/utils/logger'); // Mock logger supaya tidak perlu logging saat test dijalankan
+
+
 
 describe('User API Endpoints', () => {
   describe('POST /users', () => {
     it('should create a user and return the user data', async () => {
-
-      // Supertest request
       const response = await request(app)
-        .post('/users')
+        .post('/api/users')
+        .set('authorization', `Basic ${credentials}`)
         .send({ username: 'testuser', password: 'password', roleId: 1 });
-
-      expect(response.status).toBe(200);
+      
+      expect(response.status).toBe(201);
       expect(response.body).toHaveProperty('data');
       expect(response.body.data.username).toBe('testuser');
     });
@@ -49,23 +58,24 @@ describe('User API Endpoints', () => {
     it('should return 400 if user creation fails', async () => {
       // Mocking prisma to throw error
       const response = await request(app)
-        .post('/users')
+        .post('/api/users')
+        .set('authorization', `Basic ${credentials}`)
         .send({ username: 'testuser', password: 'password'});
 
       expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('error');
+      expect(response.body.data).toBeUndefined();
     });
   });
 
-  describe('GET /users', () => {
-    it('should return list of users', async () => {
-      // Mocking prisma getUser function
-      const response = await request(app).get('/users');
+  // describe('GET /users', () => {
+  //   it('should return list of users', async () => {
+  //     // Mocking prisma getUser function
+  //     const response = await request(app).get('/api/users');
 
-      expect(response.status).toBe(200);
-      expect(Array.isArray(response.body)).toBe(true);
-      expect(response.body.length).toBeGreaterThan(2);
-      expect(response.body[0].username).toBe('user');
-    });
-  });
+  //     expect(response.status).toBe(200);
+  //     expect(Array.isArray(response.body)).toBe(true);
+  //     expect(response.body.length).toBeGreaterThan(2);
+  //     expect(response.body[0].username).toBe('user');
+  //   });
+  // });
 });
